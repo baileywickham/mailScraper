6c2943aca7f87b1d2118c3da599e43cec8f215df
baileywickham <baileywickham@gmail.com>
2017-12-15 20:36:43 -0800

    update, still not working

--- mail.py
+++ mail.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 import imaplib
 mail = imaplib.IMAP4_SSL('imap.gmail.com')
-mail.login('bspam6969@gmail.com', 'Planes123')
+mail.login('bspam6969@gmail.com', 'Planes123')  # FILL PW IN
 mail.list()
 # Out: list of "folders" aka labels in gmail.
 mail.select("inbox")  # connect to inbox.
+subject = "You've got money"
 
-result, data = mail.search(None, "ALL")
+# result, data = mail.search(None, '(UNSEEN SUBJECT "%s")'.format(subject))
+result, data = mail.search(None, 'ALL')
 
-ids = data[0]  # data is a list.
+ids = data[0]  # data is a list, each list item should include an email
+
 id_list = ids.split()  # ids is a space separated string
 latest_email_id = id_list[-1]  # get the latest
 
@@ -16,3 +19,14 @@ result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (R
 
 raw_email = data[0][1]  # here's the body, which is raw text of the whole email
 # including headers and alternate payloads
+
+soup = BeautifulSoup(raw_email, 'html.parser')
+
+print(raw_email)
+# print(soup.get_text())
+
+# Write email to file for checking.
+with open('email.txt', 'w') as file:
+    a = soup.prettify(encoding=None)
+    # print(soup.prettify(encoding=None))
+    file.write(str(raw_email))
